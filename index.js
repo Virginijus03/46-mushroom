11893eb86a4c4456d1719f7130e4db36ff3d575e
Virginijus03 <77634732+Virginijus03@users.noreply.github.com>
2021-09-06 19:47:49 +0300

    start mushroom

--- index.js
+++ index.js
@@ -7,12 +7,37 @@ app.init = async () => {
     const connection = await mysql.createConnection({   //[rows = [], felds = []]
         host: 'localhost',
         user: 'root',
-        database: 'taxi',
+        database: 'mushroom',
     });
 
     let sql = '';
     let rows = [];
 
+    function upperName(str) {
+        return str[0].toUpperCase() + str.slice(1);
+    }
+
+    //1. Isspausdinti, visu grybu pavadinimus ir ju kainas,
+    // grybus isrikiuojant nuo brangiausio link pigiausio.
+    sql = 'SELECT `mushroom`, `price` FROM `mushroom`';
+    [rows] = await connection.execute(sql);
+    console.log(`Grybai:`);
+    let pavadinimas = '';
+    let kaina = 0;
+    for (let index = 0; index < rows.length; index++) {
+        pavadinimas = rows[index].mushroom;
+        kaina = rows[index].price;
+
+        console.log(`${index + 1}) ${upperName(pavadinimas)} - ${kaina} EUR/kg`);
+    }
+
+    //2. Isspausdinti, visu grybautoju vardus.
+    sql = 'SELECT `name` FROM `gatherer`';
+    [rows] = await connection.execute(sql);
+    const vardai = rows.map(obj => obj.name);
+    console.log(`Grybautojai: ${vardai.join(', ')}.`);
+
+    //3. Isspausdinti, brangiausio grybo pavadinima.
 
 }
 
